4dcaa0bf8fb66e16cfba0d3f55f353f47cdbf5d0
tannerbroberts <tannerbroberts.13@gmail.com>
2021-06-10 11:03:22 -0600

    Initial commit

---
+++ app/src/main/java/com/example/cs246app/Do_I_Need_A_Lawyer.java
@@ -0,0 +1,14 @@
+package com.example.cs246app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Do_I_Need_A_Lawyer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_do_ineed_alawyer);
+    }
+}
---
+++ app/src/main/java/com/example/cs246app/RequestedPhotosActivity.java
@@ -0,0 +1,14 @@
+package com.example.cs246app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RequestedPhotosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_requested_photos);
+    }
+}
