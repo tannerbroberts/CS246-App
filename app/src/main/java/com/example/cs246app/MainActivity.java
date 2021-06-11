7f51959e43270b9935688c7d4d3d025a0e2f1032
Swenson <swensonmbailey@yahoo.com>
2021-06-11 14:53:44 -0500

    Changes button id names and adds call function

--- app/src/main/java/com/example/cs246app/MainActivity.java
+++ app/src/main/java/com/example/cs246app/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.cs246app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void call(View view){
+        Uri number = Uri.parse("tel:(870)2467070");
+        Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
+
+        try{
+            startActivity(callIntent);
+        }catch (ActivityNotFoundException e){
+            Toast.makeText(MainActivity.this, "Your phone can't make this call.", Toast.LENGTH_LONG).show();
+        }
     }
 }
