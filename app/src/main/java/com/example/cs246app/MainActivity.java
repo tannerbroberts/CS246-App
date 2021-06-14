5e3d7e0669bfa03a9294a6a4f743453f57cbc915
tannerbroberts <tannerbroberts.13@gmail.com>
2021-06-14 16:02:09 -0600

    Add SMS messaging functionality

--- app/src/main/java/com/example/cs246app/MainActivity.java
+++ app/src/main/java/com/example/cs246app/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.cs246app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -15,6 +22,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.SEND_SMS}, 1);
+        }
     }
 
     public void call(View view){
@@ -25,6 +38,22 @@ public class MainActivity extends AppCompatActivity {
             startActivity(callIntent);
         }catch (ActivityNotFoundException e){
             Toast.makeText(MainActivity.this, "Your phone can't make this call.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    // This is for granting SMS permissions
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission granted!", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(this, "Well... This app won't be worth much to you...", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
