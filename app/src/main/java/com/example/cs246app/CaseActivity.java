ad9a7bd27bbf3d58474064c78232cdfd69932b03
Swenson <swensonmbailey@yahoo.com>
2021-06-23 15:14:54 -0500

    adds the ability to end a case. Adds new button to case activity layout

--- app/src/main/java/com/example/cs246app/CaseActivity.java
+++ app/src/main/java/com/example/cs246app/CaseActivity.java
@@ -2,13 +2,15 @@ package com.example.cs246app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 
 public class CaseActivity extends AppCompatActivity {
-
+    View endCaseButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +20,29 @@ public class CaseActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         setContentView(R.layout.activity_case);
+
+        endCaseButton = findViewById(R.id.endCase);
+        endCaseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                endCase();
+            }
+        });
     }
 
     public void openPainJournalActivity(View view) {
         Intent intent = new Intent(this, PainJournalActivity.class);
         startActivity(intent);
     }
+
+    public void endCase(){
+        SharedPreferences data = getSharedPreferences("com.example.cs246app.data", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = data.edit();
+        editor.putBoolean("isCaseActive", false);
+        editor.apply();
+        this.finish();
+    }
+
 
 
 }
