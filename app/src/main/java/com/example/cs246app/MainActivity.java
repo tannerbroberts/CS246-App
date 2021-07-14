ad2f4cf5966be5768df4a716ec5bb0374629ca7c
Pearl <cembr7@protonmail.com>
2021-07-14 16:06:57 -0700

    Notifications working!

--- app/src/main/java/com/example/cs246app/MainActivity.java
+++ app/src/main/java/com/example/cs246app/MainActivity.java
@@ -28,10 +28,41 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Channel Name";
+            String description = "Channel Description";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("painJournal", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        createNotificationChannel();
+        Intent notificationIntent = new Intent();
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        // Builds Notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "painJournal" )
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Pain Journal Reminder")
+                .setContentText("Don't forget to record and submit your pain journal entry.")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                //.setContentIntent(contentIntent)
+                .setAutoCancel(true);
+        builder.setContentIntent(contentIntent);
+        // Add as notification
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(0, builder.build());
 
         //takes away the title from the top of the page
         requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -39,68 +70,14 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+
         if (ContextCompat.checkSelfPermission(MainActivity.this,
                 Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainActivity.this,
                     new String[]{Manifest.permission.SEND_SMS}, 1);
         }
-        Button createNotificationButton = findViewById(R.id.button_notification);
 
-            // private void createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            // CharSequence name = getString(R.string.channel_name);
-            // String description = getString(R.string.channel_discription);
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel("Notification", "Notification", NotificationManager.IMPORTANCE_DEFAULT);
-            // channel.setDescription(description);
-            // NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            // NotificationManager.createNotificationChannel(channel);
-
-            // NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
-            // managerCompat.notify(0, builder.build());
-        }
-
-        createNotificationButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // addNotification();
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, "Notification" );
-                        builder.setSmallIcon(R.drawable.ic_launcher_background);
-                        builder.setContentTitle("Pain Journal reminder");
-                        builder.setContentText("Pain Journal entry needed");
-                        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
-//                        builder.setContentIntent(pendingIntent);
-                        builder.setAutoCancel(true);
-
-                NotificationManagerCompat managerCompat = NotificationManagerCompat.from(MainActivity.this);
-                managerCompat.notify(0, builder.build());
-
-
-            }
-        });
     }
-
-    /* private void addNotification() {
-        // Builds Notification
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "Notification" )
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentTitle("Pain Journal reminder")
-                .setContentText("Pain Journal entry needed")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                // .setContentIntent(pendingIntent)
-                .setAutoCancel(true);
-
-
-
-        // Creates intent needed to show the notification
-        Intent notificationIntent = new Intent(this, MainActivity.class);
-        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        builder.setContentIntent(contentIntent);
-
-        // Add as notification
-        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        manager.notify(0, builder.build());
-    } */
 
 
 
