5e20e1dc9367c2fbcede94903dd3097276d16f7a
Swenson <swensonmbailey@yahoo.com>
2021-07-14 19:22:55 -0500

    Adds firstName and lastName to SharedPreferences

--- app/src/main/java/com/example/cs246app/StartCase.java
+++ app/src/main/java/com/example/cs246app/StartCase.java
@@ -8,6 +8,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class StartCase extends AppCompatActivity {
     @Override
@@ -24,14 +26,26 @@ public class StartCase extends AppCompatActivity {
     }
 
     public void startTheCase(View view){
+        TextView first = findViewById(R.id.firstName);
+        String firstName = first.getText().toString();
+        TextView last = findViewById(R.id.lastName);
+        String lastName = last.getText().toString();
 
-        SharedPreferences data = getSharedPreferences("com.example.cs246app.data", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = data.edit();
-        editor.putBoolean("isCaseActive", true);
-        editor.apply();
-        this.finish();
-        Intent intent = new Intent(this, CaseActivity.class);
-        startActivity(intent);
+        if(firstName.length() > 0 && lastName.length() > 0){
+            SharedPreferences data = getSharedPreferences("com.example.cs246app.data", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = data.edit();
+            editor.putBoolean("isCaseActive", true);
+            editor.putString("firstName", firstName.toUpperCase());
+            editor.putString("lastName", lastName.toUpperCase());
+            editor.apply();
+            this.finish();
+            Intent intent = new Intent(this, CaseActivity.class);
+            startActivity(intent);
+        }else{
+            Toast.makeText(this, "Please Enter Your First and Last Name", Toast.LENGTH_SHORT).show();
+        }
+
+
 
     }
 }
